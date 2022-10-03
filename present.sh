6f92088c1fe05ded9fc635f1527619fcfed3d568
piotrjwegrzyn <piotrwegrzyn@protonmail.com>
2022-10-03 22:14:07 +0200

    Show hexdump for interfaces

---
+++ present.sh
@@ -0,0 +1,16 @@
+if [ $# -ne 1 ];
+then
+    exit 1
+fi
+
+ip addr show $1 &> /dev/null
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
+
+xxd -p -c 16 interfaces/$1 2> /dev/null
+# Alternatively:
+# xxd interfaces/$1
+# hexdump -v -C interfaces/$1
+# hexdump -v interfaces/$1
---
+++ update.sh
@@ -0,0 +1,1 @@
+ls interfaces/ | cat
